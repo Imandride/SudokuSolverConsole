8254755a8e317ed88ff020a829546118e48c6f05
pablok2 <pgorelov@gmail.com>
2015-08-13 17:27:08 -0400

    Added files for the cpp console app

---
+++ SudokuSolver/SudokuSolver.cpp
@@ -0,0 +1,18 @@
+// SudokuSolver.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World!\n";
+
+	int i;
+	cin >> i;
+
+    return 0;
+}
+
